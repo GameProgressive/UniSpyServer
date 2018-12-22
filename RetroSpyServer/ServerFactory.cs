94ce43ab6c2cf86d2d53e7de1a91ff6d1be62815
xiaojiuwo <koujiangheng@live.cn>
2018-12-22 10:21:31 +0800

    folder structure changes and some minor changes

--- RetroSpyServer/ServerFactory.cs
+++ RetroSpyServer/ServerFactory.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace RetroSpyServer
 {
+    /// <summary>
+    /// a server factory that create the instance of  servers
+    /// </summary>
     public class ServerFactory
     {
 
