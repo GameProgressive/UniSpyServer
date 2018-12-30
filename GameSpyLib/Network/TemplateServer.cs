97b73d4671cf33a22460cc7641a6ec572ec6143a
Arves100 <arves100@gmail.com>
2018-12-30 21:22:08 +0100

    Removed Common folder, otherwise Mono won't build it

--- GameSpyLib/Network/TemplateServer.cs
+++ GameSpyLib/Network/TemplateServer.cs
@@ -1,38 +1,39 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using GameSpyLib.Gamespy.Net;
+using GameSpyLib.Database;
 
-namespace GameSpyLib.Net
+namespace GameSpyLib.Network
 {
     /// <summary>
-    /// This class represents a high performance Async Tcp Socket wrapper
-    /// that is used to act as a base for all Gamespy protocol Tcp
-    /// connections.
+    /// This class rapresents an implementable server
     /// </summary>
-    public abstract class TcpServer : IDisposable
+    public abstract class TemplateServer : IDisposable
     {
         /// <summary>
-        /// Max number of concurrent open and active connections. Increasing this number
-        /// will also increase the IO Buffer by <paramref name="BufferSizePerOperation"/> * 2
+        /// A connection to a database
         /// </summary>
-        protected readonly int MaxNumConnections;
+        protected DatabaseDriver databaseDriver = null;
 
         /// <summary>
-        /// The initial size of the conccurent accept pool
-        /// when accepting new connections. High volume of connections
-        /// will increase the pool size if need be
-        /// <remarks>4 should be a pretty good init compacity since accepting a client is pretty fast</remarks>
+        /// Indicates whether the server is still running, and not in the process of shutting down
         /// </summary>
-        protected readonly int ConcurrentAcceptPoolSize = 4;
+        public bool IsRunning { get; protected set; }
 
         /// <summary>
-        /// The amount of bytes each SocketAysncEventArgs object
-        /// will get assigned to in the buffer manager.
+        /// Indicates whether this object has been disposed yet
         /// </summary>
-        protected readonly int BufferSizePerOperation = 256;
+        public bool IsDisposed { get; protected set; }
+
+        /// <summary>
+        /// The port we are listening on
+        /// </summary>
+        public int Port { get; protected set; }
+
+        /// <summary>
+        /// The IP Address we are listening on
+        /// </summary>
+        public string Address { get; protected set; }
 
         /// <summary>
         /// Our Listening Socket
@@ -40,358 +41,60 @@ namespace GameSpyLib.Net
         protected Socket Listener;
 
         /// <summary>
-        /// Buffers for sockets are unmanaged by .NET, which means that
-        /// memory will get fragmented because the GC can't touch these
-        /// byte arrays. So we will manage our buffers manually
+        /// Max number of concurrent open and active connections.
         /// </summary>
-        protected BufferManager BufferManager;
+        protected int MaxNumConnections;
 
         /// <summary>
-        /// Use a Semaphore to prevent more then the MaxNumConnections
-        /// clients from logging in at once.
+        /// Constructor
         /// </summary>
-        protected SemaphoreSlim MaxConnectionsEnforcer;
-
-        /// <summary>
-        /// Determines where the MaxConnectionsEnforcer stops to wait for a spot to open, and allow
-        /// the connecting client through.
-        /// </summary>
-        protected EnforceMode ConnectionEnforceMode = EnforceMode.BeforeAccept;
-
-        /// <summary>
-        /// If the ConnectionEnforceMode is set to DuringPrepare, then the wait timeout is set here
-        /// </summary>
-        protected int WaitTimeout = 500;
-
-        /// <summary>
-        /// The error message to display if the server is full. Only works if ConnectionEnforceMode is set to DuringPrepare.
-        /// If left empty, no message will be sent back to the client
-        /// </summary>
-        protected string FullErrorMessage = String.Empty;
-
-        /// <summary>
-        /// A pool of reusable SocketAsyncEventArgs objects for accept operations
-        /// </summary>
-        protected SocketAsyncEventArgsPool SocketAcceptPool;
-
-        /// <summary>
-        /// A pool of reusable SocketAsyncEventArgs objects for receive and send socket operations
-        /// </summary>
-        protected SocketAsyncEventArgsPool SocketReadWritePool;
-        
-        /// <summary>
-        /// Indicates whether the socket is listening for connections
-        /// </summary>
-        public bool IsListening { get; protected set; }
-
-        /// <summary>
-        /// If set to True, new connections will be immediatly closed and ignored.
-        /// </summary>
-        public bool IgnoreNewConnections { get; protected set; } = false;
-
-        /// <summary>
-        /// Indicates whether this object has been disposed yet
-        /// </summary>
-        public bool IsDisposed { get; protected set; } = false;
-
-        public GamespyTcpSocket(IPEndPoint bindTo, int MaxConnections)
+        /// <param name="databaseDriver">
+        /// A connection to a database that could be used in the server.
+        /// Set this parameter to null if the server does not require a database connection.
+        /// </param>
+        public TemplateServer(DatabaseDriver databaseDriver)
         {
-            // Create our Socket
-            Listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            this.databaseDriver = databaseDriver;
 
-            // Set Socket options
-            Listener.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontLinger, true);
-            Listener.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Linger, false);
-            Listener.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
-
-            // Bind to our port
-            Listener.Bind(bindTo);
-            Listener.Listen(25);
-
-            // Set the rest of our internals
-            MaxNumConnections = MaxConnections;
-            MaxConnectionsEnforcer = new SemaphoreSlim(MaxNumConnections, MaxNumConnections);
-            SocketAcceptPool = new SocketAsyncEventArgsPool(ConcurrentAcceptPoolSize);
-            SocketReadWritePool = new SocketAsyncEventArgsPool(MaxNumConnections * 2);
-
-            // Create our Buffer Manager for IO operations. 
-            // Always allocate double space, one for recieving, and another for sending
-            BufferManager = new BufferManager(MaxNumConnections * 2, BufferSizePerOperation);
-
-            // Assign our Connection Accept SocketAsyncEventArgs object instances
-            for (int i = 0; i < ConcurrentAcceptPoolSize; i++)
-            {
-                SocketAsyncEventArgs SockArg = new SocketAsyncEventArgs();
-                SockArg.Completed += (s, e) => PrepareAccept(e);
-
-                // Do NOT assign buffer space for Accept operations!               
-                SocketAcceptPool.Push(SockArg);
-            }
-
-            // Assign our Connection IO SocketAsyncEventArgs object instances
-            for (int i = 0; i < MaxNumConnections * 2; i++)
-            {
-                SocketAsyncEventArgs SockArg = new SocketAsyncEventArgs();
-                BufferManager.AssignBuffer(SockArg);
-                SocketReadWritePool.Push(SockArg);
-            }
-
-            // set public internals
-            IsListening = true;
+            IsRunning = false;
+            IsDisposed = true;
         }
 
-        ~GamespyTcpSocket()
+        /// <summary>
+        /// Default distructor
+        /// </summary>
+        ~TemplateServer()
         {
             if (!IsDisposed)
                 Dispose();
         }
 
         /// <summary>
-        /// Releases all Objects held by this socket. Will also
-        /// shutdown the socket if its still running
+        /// Stops the server
         /// </summary>
-        public void Dispose()
+        public abstract void Stop();
+
+        /// <summary>
+        /// Starts the server
+        /// </summary>
+        /// <param name="ip">The IP to bind</param>
+        /// <param name="port">The port to bind</param>
+        /// <param name="maxConnections">Max connections allowed</param>
+        public void Start(string ip, int port, int maxConnections)
         {
-            // no need to do this again
-            if (IsDisposed) return;
-            IsDisposed = true;
-
-            // Shutdown sockets
-            if (IsListening)
-                ShutdownSocket();
-
-            // Dispose all AcceptPool AysncEventArg objects
-            while (SocketAcceptPool.Count > 0)
-                SocketAcceptPool.Pop().Dispose();
-
-            // Dispose all ReadWritePool AysncEventArg objects
-            while (SocketReadWritePool.Count > 0)
-                SocketReadWritePool.Pop().Dispose();
-
-            // Dispose the buffer manager after disposing all EventArgs
-            BufferManager?.Dispose();
-            MaxConnectionsEnforcer?.Dispose();
-            Listener?.Dispose();
+            Start(new IPEndPoint(IPAddress.Parse(ip), port), maxConnections);
         }
 
         /// <summary>
-        /// When called, this method will stop accepted and handling any and all
-        /// connections. Dispose still needs to be called!
+        /// Starts the server
         /// </summary>
-        protected void ShutdownSocket()
-        {
-            // Only do this once
-            if (!IsListening) return;
-            IsListening = false;
-
-            // Stop accepting connections
-            try
-            {
-                Listener.Shutdown(SocketShutdown.Both);
-            }
-            catch { }
-
-            // Close the listener
-            Listener.Close();
-        }
+        /// <param name="endPoint">The IP Endpoint to bind the server</param>
+        /// <param name="maxConnections">Max connections allowed</param>
+        public abstract void Start(IPEndPoint endPoint, int maxConnections);
 
         /// <summary>
-        /// Releases the Stream's SocketAsyncEventArgs back to the pool,
-        /// and free's up another slot for a new client to connect
+        /// Releases all objects created by the server
         /// </summary>
-        /// <param name="Stream">The GamespyTcpStream object that is being released.</param>
-        public void Release(GamespyTcpStream Stream)
-        {
-            // If the stream has been released, then we stop here
-            if (!IsListening || Stream.Released) return;
-
-            // Make sure the connection is closed properly
-            if (!Stream.SocketClosed)
-            {
-                Stream.Close();
-                return;
-            }
-
-            // To prevent cross instance releasing
-            //if (!Object.ReferenceEquals(this, Stream.SocketManager))
-            //throw new ArgumentException("Cannot pass a GamespyTcpStream belonging to a different TcpSocket than this one.");
-
-            // If we are still registered for this event, then the EventArgs should
-            // NEVER be disposed here, or we have an error to fix
-            if (Stream.DisposedEventArgs)
-            {
-                // Dispose old buffer tokens
-                BufferManager.ReleaseBuffer(Stream.ReadEventArgs);
-                BufferManager.ReleaseBuffer(Stream.WriteEventArgs);
-
-                // Create new Read Event Args
-                SocketAsyncEventArgs SockArgR = new SocketAsyncEventArgs();
-                BufferManager.AssignBuffer(SockArgR);
-                SocketReadWritePool.Push(SockArgR);
-
-                // Create new Write Event Args
-                SocketAsyncEventArgs SockArgW = new SocketAsyncEventArgs();
-                BufferManager.AssignBuffer(SockArgW);
-                SocketReadWritePool.Push(SockArgW);
-            }
-            else
-            {
-                // Set null's
-                Stream.ReadEventArgs.AcceptSocket = null;
-                Stream.WriteEventArgs.AcceptSocket = null;
-
-                // Get our ReadWrite AsyncEvent object back
-                SocketReadWritePool.Push(Stream.ReadEventArgs);
-                SocketReadWritePool.Push(Stream.WriteEventArgs);
-            }
-
-            // Now that we have another set of AsyncEventArgs, we can
-            // release this users Semephore lock, allowing another connection
-            MaxConnectionsEnforcer.Release();
-        }
-
-        /// <summary>
-        /// Begins accepting a new Connection asynchronously
-        /// </summary>
-        protected async void StartAcceptAsync()
-        {
-            // Fetch ourselves an available AcceptEventArg for the next connection
-            SocketAsyncEventArgs AcceptEventArg;
-            if (SocketAcceptPool.Count > 0)
-            {
-                try
-                {
-                    AcceptEventArg = SocketAcceptPool.Pop();
-                }
-                catch
-                {
-                    AcceptEventArg = new SocketAsyncEventArgs();
-                    AcceptEventArg.Completed += (s, e) => PrepareAccept(e);
-                }
-            }
-            else
-            {
-                // NO SOCKS AVAIL!
-                AcceptEventArg = new SocketAsyncEventArgs();
-                AcceptEventArg.Completed += (s, e) => PrepareAccept(e);
-            }
-
-            try
-            {
-                // Enforce max connections. If we are capped on connections, the new connection will stop here,
-                // and retrun once a connection is opened up from the Release() method
-                if (ConnectionEnforceMode == EnforceMode.BeforeAccept)
-                    await MaxConnectionsEnforcer.WaitAsync();
-
-                // Begin accpetion connections
-                bool willRaiseEvent = Listener.AcceptAsync(AcceptEventArg);
-
-                // If we wont raise event, that means a connection has already been accepted syncronously
-                // and the Accept_Completed event will NOT be fired. So we manually call ProcessAccept
-                if (!willRaiseEvent)
-                    PrepareAccept(AcceptEventArg);
-            }
-            catch (ObjectDisposedException)
-            {
-                // Happens when the server is shutdown
-            }
-        }
-
-        /// <summary>
-        /// Once a connection has been received, its handed off here to convert it into
-        /// our client object, and prepared to be handed off to the parent for processing
-        /// </summary>
-        /// <param name="AcceptEventArg"></param>
-        protected async void PrepareAccept(SocketAsyncEventArgs AcceptEventArg)
-        {
-            // If we do not get a success code here, we have a bad socket
-            if (IgnoreNewConnections || AcceptEventArg.SocketError != SocketError.Success)
-            {
-                // This method closes the socket and releases all resources, both
-                // managed and unmanaged. It internally calls Dispose.           
-                AcceptEventArg.AcceptSocket.Close();
-
-                // Put the SAEA back in the pool.
-                SocketAcceptPool.Push(AcceptEventArg);
-                StartAcceptAsync();
-                return;
-            }
-
-            // If the server is full, send an error message to the player
-            if (ConnectionEnforceMode == EnforceMode.DuringPrepare)
-            {
-                bool Success = await MaxConnectionsEnforcer.WaitAsync(WaitTimeout);
-                if (!Success)
-                {
-                    // If we arent even listening...
-                    if (!IsListening) return;
-
-                    // Alert the client that we are full
-                    if (!String.IsNullOrEmpty(FullErrorMessage))
-                    {
-                        byte[] buffer = Encoding.UTF8.GetBytes(
-                            String.Format(@"\error\\err\0\fatal\\errmsg\{0}\id\1\final\", FullErrorMessage)
-                        );
-                        AcceptEventArg.AcceptSocket.Send(buffer);
-                    }
-
-                    // Log so we can track this!
-                    //Program.ErrorLog.Write("NOTICE: [GamespyTcpSocket.PrepareAccept] The Server is currently full! Rejecting connecting client.");
-
-                    // Put the SAEA back in the pool.
-                    AcceptEventArg.AcceptSocket.Close();
-                    SocketAcceptPool.Push(AcceptEventArg);
-                    StartAcceptAsync();
-                    return;
-                }
-            }
-
-            // Begin accepting a new connection
-            StartAcceptAsync();
-
-            // Grab a send/recieve object
-            SocketAsyncEventArgs ReadArgs = SocketReadWritePool.Pop();
-            SocketAsyncEventArgs WriteArgs = SocketReadWritePool.Pop();
-
-            // Pass over the reference to the new socket that is handling
-            // this specific stream, and dereference it so we can hand the
-            // acception event back over
-            ReadArgs.AcceptSocket = WriteArgs.AcceptSocket = AcceptEventArg.AcceptSocket;
-            AcceptEventArg.AcceptSocket = null;
-
-            // Hand back the AcceptEventArg so another connection can be accepted
-            SocketAcceptPool.Push(AcceptEventArg);
-
-            // Hand off processing to the parent
-            GamespyTcpStream Stream = null;
-            try
-            {
-                Stream = new GamespyTcpStream(this, ReadArgs, WriteArgs);
-                ProcessAccept(Stream);
-            }
-            catch (Exception e)
-            {
-                // Report Error
-                OnException(e);
-
-                // Make sure the connection is closed properly
-                if (Stream != null)
-                    Release(Stream);
-            }
-        }
-
-        /// <summary>
-        /// When a new connection is established, the parent class is responsible for
-        /// processing the connected client
-        /// </summary>
-        /// <param name="Stream">A GamespyTcpStream object that wraps the I/O AsyncEventArgs and socket</param>
-        protected abstract void ProcessAccept(GamespyTcpStream Stream);
-
-        protected abstract void OnException(Exception e);
-    }
-
-    public enum EnforceMode
-    {
-        BeforeAccept, DuringPrepare
+        public abstract void Dispose();
     }
 }
--- RetroSpyServer/Server/GPCMServer.cs
+++ RetroSpyServer/Server/GPCMServer.cs
@@ -3,26 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using GameSpyLib.Database;
 using GameSpyLib.Network;
 using GameSpyLib.Server;
+using GameSpyLib.Logging;
 
 namespace RetroSpyServer.Server
 {
     public class GPCMServer : PresenceServer
     {
+        /// <summary>
+        /// A connection counter, used to create unique connection id's
+        /// </summary>
+        private long ConnectionCounter = 0;
+
+        /// <summary>
+        /// Indicates the timeout of when a connecting client will be disconnected
+        /// </summary>
+        public const int Timeout = 20000;
+
         public GPCMServer(DatabaseDriver databaseDriver) : base(databaseDriver)
         {
         }
 
         protected override void OnException(Exception e)
         {
-            throw new NotImplementedException();
+            throw e;
         }
 
         protected override void ProcessAccept(TCPStream Stream)
         {
-            throw new NotImplementedException();
+            long connectionId = Interlocked.Increment(ref ConnectionCounter);
+            /*GPCMClient client;
+
+            try
+            {
+                client = new GPCMClient(Stream, connectionId);
+                Processing.TryAdd(connectionId, client);
+
+                client.SendServerChallenge();
+            }
+            catch (Exception e)
+            {
+                Processing.TryRemove(connectionId, out client);
+                base.Release(Stream);
+            }*/
         }
     }
 }
