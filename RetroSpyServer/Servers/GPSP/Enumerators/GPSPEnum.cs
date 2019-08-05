15873ad557d72cf97cf298d5f46264730d67444e
XIAOJIUWO <koujiangheng@live.cn>
2019-08-05 11:57:05 +0800

    Updated all enum to C# style

--- RetroSpyServer/Servers/GPSP/Enumerators/GPSPEnum.cs
+++ RetroSpyServer/Servers/GPSP/Enumerators/GPSPEnum.cs
@@ -4,201 +4,201 @@ using System.Text;
 
 namespace RetroSpyServer.Servers.GPSP.Enumerators
 {
-    public enum GPSPEnum:uint
+    public enum GPSPEnum : uint
     {
         // Callbacks
         ////////////
-        GP_ERROR = 0,
-        GP_RECV_BUDDY_REQUEST,
-        GP_RECV_BUDDY_STATUS,
-        GP_RECV_BUDDY_MESSAGE,
-        GP_RECV_BUDDY_UTM,
-        GP_RECV_GAME_INVITE,
-        GP_TRANSFER_CALLBACK,
-        GP_RECV_BUDDY_AUTH,
-        GP_RECV_BUDDY_REVOKE,
+        Error = 0,
+        RecvBuddyRequest,
+        RecvBuddyStatus,
+        RecvBuddyMessage,
+        RecvBuddyUTM,
+        RecvGameInvite,
+        TransferCallback,
+        RecvBuddyAuth,
+        RecvBuddyRevoke,
 
         // Global States.
         /////////////////
-        GP_INFO_CACHING = 0x0100,
-        GP_SIMULATION,
-        GP_INFO_CACHING_BUDDY_AND_BLOCK_ONLY,
+        InfoCaching = 0x0100,
+        Simulation,
+        InfoChachingBuddyAndBlockOnly,
 
         // Blocking
         ///////////
-        GP_BLOCKING = 1,
-        GP_NON_BLOCKING = 0,
+        Blocking = 1,
+        NonBlocking = 0,
 
         // Firewall
         ///////////
-        GP_FIREWALL = 1,
-        GP_NO_FIREWALL = 0,
+        Firewall = 1,
+        NoFirewall = 0,
 
         // Check Cache
         //////////////
-        GP_CHECK_CACHE = 1,
-        GP_DONT_CHECK_CACHE = 0,
+        CheckCache = 1,
+        DontCheckCache = 0,
 
         // Is Valid Email.
         // PANTS|02.15.00
         //////////////////
-        GP_VALID = 1,
-        GP_INVALID = 0,
+        EmailValid = 1,
+        EmailInvalid = 0,
 
         // Fatal Error.
         ///////////////
-        GP_FATAL = 1,
-        GP_NON_FATAL = 0,
+        Fatal = 1,
+        NonFatal = 0,
 
         // Sex
         //////
-        GP_MALE = 0x0500,
-        GP_FEMALE,
-        GP_PAT,
+        Male = 0x0500,
+        Fefamle,
+        PAT,
 
         // Profile Search.
         //////////////////
-        GP_MORE = 0x0600,
-        GP_DONE,
+        More = 0x0600,
+        Done,
 
         // Set Info
         ///////////
-        GP_NICK = 0x0700,
-        GP_UNIQUENICK,
-        GP_EMAIL,
-        GP_PASSWORD,
-        GP_FIRSTNAME,
-        GP_LASTNAME,
-        GP_ICQUIN,
-        GP_HOMEPAGE,
-        GP_ZIPCODE,
-        GP_COUNTRYCODE,
-        GP_BIRTHDAY,
-        GP_SEX,
-        GP_CPUBRANDID,
-        GP_CPUSPEED,
-        GP_MEMORY,
-        GP_VIDEOCARD1STRING,
-        GP_VIDEOCARD1RAM,
-        GP_VIDEOCARD2STRING,
-        GP_VIDEOCARD2RAM,
-        GP_CONNECTIONID,
-        GP_CONNECTIONSPEED,
-        GP_HASNETWORK,
-        GP_OSSTRING,
-        GP_AIMNAME,  // PANTS|03.20.01
-        GP_PIC,
-        GP_OCCUPATIONID,
-        GP_INDUSTRYID,
-        GP_INCOMEID,
-        GP_MARRIEDID,
-        GP_CHILDCOUNT,
-        GP_INTERESTS1,
+        Nick = 0x0700,
+        Uniquenick,
+        Email,
+        Password,
+        FirstName,
+        LastName,
+        ICQUIN,
+        HomePage,
+        ZIPCode,
+        CountryCode,
+        Birthday,
+        Sex,
+        CPUBrand,
+        CPUSpeed,
+        Memory,
+        VideoCard1String,
+        VideoCard1RAM,
+        VideoCard2String,
+        VideoCard2RAM,
+        ConnectionID,
+        ConnectionSpeed,
+        HasNetwork,
+        OSString,
+        AIMName,  // PANTS|03.20.01
+        PIC,
+        OccupationID,
+        IndustryID,
+        InComeID,
+        MarriedID,
+        ChildCount,
+        Interest1,
 
         // New Profile.
         ///////////////
-        GP_REPLACE = 1,
-        GP_DONT_REPLACE = 0,
+        Replace = 1,
+        DontReplace = 0,
 
         // Is Connected.
         ////////////////
-        GP_CONNECTED = 1,
-        GP_NOT_CONNECTED = 0,
+        Connected = 1,
+        NotConnected = 0,
 
         // Public mask.
         ///////////////
-        GP_MASK_NONE = 0x00000000,
-        GP_MASK_HOMEPAGE = 0x00000001,
-        GP_MASK_ZIPCODE = 0x00000002,
-        GP_MASK_COUNTRYCODE = 0x00000004,
-        GP_MASK_BIRTHDAY = 0x00000008,
-        GP_MASK_SEX = 0x00000010,
-        GP_MASK_EMAIL = 0x00000020,
-        GP_MASK_ALL = 0xFFFFFFFF,
+        MaskNone = 0x00000000,
+        MaskHomepage = 0x00000001,
+        MaskZIPCode = 0x00000002,
+        MaskContryCode = 0x00000004,
+        MaskBirthday = 0x00000008,
+        MaskSex = 0x00000010,
+        MaskEmail = 0x00000020,
+        MaskAll = 0xFFFFFFFF,
 
         // Status
         /////////
-        GP_OFFLINE = 0,
-        GP_ONLINE = 1,
-        GP_PLAYING = 2,
-        GP_STAGING = 3,
-        GP_CHATTING = 4,
-        GP_AWAY = 5,
+        Offline = 0,
+        Online = 1,
+        Playing = 2,
+        Staging = 3,
+        Chatting = 4,
+        Away = 5,
 
         // Session flags
         /////////////////
-        GP_SESS_IS_CLOSED = 0x00000001,
-        GP_SESS_IS_OPEN = 0x00000002,
-        GP_SESS_HAS_PASSWORD = 0x00000004,
-        GP_SESS_IS_BEHIND_NAT = 0x00000008,
-        GP_SESS_IS_RANKED = 0x000000010,
+        SessIsClosed = 0x00000001,
+        SessIsOpen = 0x00000002,
+        SessHasPassword = 0x00000004,
+        SessIsBehindNAT = 0x00000008,
+        SessIsRanked = 0x000000010,
 
 
         // CPU Brand ID
         ///////////////
-        GP_INTEL = 1,
-        GP_AMD,
-        GP_CYRIX,
-        GP_MOTOROLA,
-        GP_ALPHA,
+        Intel = 1,
+        AMD,
+        CYRIX,
+        Motorola,
+        Alpha,
 
         // Connection ID.
         /////////////////
-        GP_MODEM = 1,
-        GP_ISDN,
-        GP_CABLEMODEM,
-        GP_DSL,
-        GP_SATELLITE,
-        GP_ETHERNET,
-        GP_WIRELESS,
+        Modem = 1,
+        ISDN,
+        CableModem,
+        DSL,
+        Satellite,
+        Ethernet,
+        Wireless,
 
         // Transfer callback type.
         // *** the transfer is ended when these types are received
         //////////////////////////
-        GP_TRANSFER_SEND_REQUEST = 0x800,  // arg->num == numFiles
-        GP_TRANSFER_ACCEPTED,
-        GP_TRANSFER_REJECTED,        // ***
-        GP_TRANSFER_NOT_ACCEPTING,   // ***
-        GP_TRANSFER_NO_CONNECTION,   // ***
-        GP_TRANSFER_DONE,            // ***
-        GP_TRANSFER_CANCELLED,       // ***
-        GP_TRANSFER_LOST_CONNECTION, // ***
-        GP_TRANSFER_ERROR,           // ***
-        GP_TRANSFER_THROTTLE,  // arg->num == Bps
-        GP_FILE_BEGIN,
-        GP_FILE_PROGRESS,  // arg->num == numBytes
-        GP_FILE_END,
-        GP_FILE_DIRECTORY,
-        GP_FILE_SKIP,
-        GP_FILE_FAILED,  // arg->num == error
+        TransferSendRequest = 0x800,  // arg->num == numFiles
+        TransferAccepted,
+        TransferRejected,        // ***
+        TransferNotAccepting,   // ***
+        TransferNoConnection,   // ***
+        TransferDone,            // ***
+        TransferCancelled,       // ***
+        TransferLostConnection, // ***
+       TransferError,           // ***
+        TransferThrottle,  // arg->num == Bps
+        FileBegin,
+        FileProgress,  // arg->num == numBytes
+        FileEnd,
+        FileDirectory,
+        FileSkip,
+        FileFaild,  // arg->num == error
 
-        // GP_FILE_FAILED error
+        //FILE_FAILED error
         ///////////////////////
-        GP_FILE_READ_ERROR = 0x900,
-        GP_FILE_WRITE_ERROR,
-        GP_FILE_DATA_ERROR,
+        FileReadError = 0x900,
+        FileWriteError,
+        FileDataError,
 
         // Transfer Side.
         /////////////////
-        GP_TRANSFER_SENDER = 0xA00,
-        GP_TRANSFER_RECEIVER,
+        TransferSender = 0xA00,
+        TansferReciever,
 
         // UTM send options.
         ////////////////////
-        GP_DONT_ROUTE = 0xB00, // only send direct
+        DontRout = 0xB00, // only send direct
 
         // Quiet mode flags.
         ////////////////////
-        GP_SILENCE_NONE = 0x00000000,
-        GP_SILENCE_MESSAGES = 0x00000001,
-        GP_SILENCE_UTMS = 0x00000002,
-        GP_SILENCE_LIST = 0x00000004, // includes requests, auths, and revokes
-        GP_SILENCE_ALL = 0xFFFFFFFF,
+        SlienceNone = 0x00000000,
+        SlienceMessage = 0x00000001,
+        SlienceUTMS = 0x00000002,
+        SlienceList = 0x00000004, // includes requests, auths, and revokes
+        SlienceAll = 0xFFFFFFFF,
 
         NewStatusInfoSupported = 0xC00,
         NewStatusInfoNotSupported = 0xC01
     }
-    public enum GPSPResult:int
+    public enum GPSPResult : int
     {
         NoError,
         MemoryError,
@@ -209,7 +209,7 @@ namespace RetroSpyServer.Servers.GPSP.Enumerators
         Count
     }
 
-    public enum GPSPNewUser:uint
+    public enum GPSPNewUser : uint
     {
         NewUser = 0x0200,
         NewUserBadNick,
@@ -218,7 +218,7 @@ namespace RetroSpyServer.Servers.GPSP.Enumerators
         NewUserUniqueNickINUSE,
     }
 
-    public enum GPErrorCode:uint
+    public enum GPErrorCode : uint
     {
         // General.
         ///////////
@@ -252,7 +252,7 @@ namespace RetroSpyServer.Servers.GPSP.Enumerators
         NewUserBadNick,
         NewUserBadPasswords,
         NewUserUniquenickInvalid,
-        NewUserUniquenickINUSE,        
+        NewUserUniquenickINUSE,
 
         // Updateui.
         ////////////
@@ -261,90 +261,90 @@ namespace RetroSpyServer.Servers.GPSP.Enumerators
 
         // Newprofile.
         //////////////
-        GP_NEWPROFILE = 0x0400,
-        GP_NEWPROFILE_BAD_NICK,
-        GP_NEWPROFILE_BAD_OLD_NICK,
+        NewProfile = 0x0400,
+        NewProfileBadnick,
+        NewProfileBadOldNick,
 
         // Updatepro.
         /////////////
-        GP_UPDATEPRO = 0x0500,
-        GP_UPDATEPROz_BAD_NICK,
+        UpdatePro = 0x0500,
+        UpdatePROBadNick,
 
         // Addbuddy.
         ////////////
-        GP_ADDBUDDY = 0x0600,
-        GP_ADDBUDDY_BAD_FROM,
-        GP_ADDBUDDY_BAD_NEW,
-        GP_ADDBUDDY_ALREADY_BUDDY,
+        AddBuddy = 0x0600,
+        AddBuddyBadForm,
+        AddBuddyBadNew,
+        AddBuddyAlreadyBuddy,
 
         // Authadd.
         ///////////
-        GP_AUTHADD = 0x0700,
-        GP_AUTHADD_BAD_FROM,
-        GP_AUTHADD_BAD_SIG,
+        AuthAdd = 0x0700,
+        AuthAddBadForm,
+        AuthAddBadSig,
 
         // Status.
         //////////
-        GP_STATUS = 0x0800,
+        Status = 0x0800,
 
         // Bm.
         //////
-        GP_BM = 0x0900,
-        GP_BM_NOT_BUDDY,
-        GP_BM_EXT_INFO_NOT_SUPPORTED,
-        GP_BM_BUDDY_OFFLINE,
+        Bm = 0x0900,
+        BmNotBuddy,
+        BmExtInfoNotSupported,
+        BmBuddyOffline,
 
         // Getprofile.
         //////////////
-        GP_GETPROFILE = 0x0A00,
-        GP_GETPROFILE_BAD_PROFILE,
+        GetProfile = 0x0A00,
+        GetProfileBadProfile,
 
         // Delbuddy.
         ////////////
-        GP_DELBUDDY = 0x0B00,
-        GP_DELBUDDY_NOT_BUDDY,
+        DelBuddy = 0x0B00,
+        DelBuddyNotBuddy,
 
         // Delprofile.
         /////////////
-        GP_DELPROFILE = 0x0C00,
-        GP_DELPROFILE_LAST_PROFILE,
+        DelProfile = 0x0C00,
+        DelProfileLastProfile,
 
         // Search.
         //////////
-        GP_SEARCH = 0x0D00,
-        GP_SEARCH_CONNECTION_FAILED,
-        GP_SEARCH_TIMED_OUT,
+        Search = 0x0D00,
+        SearchConnectionFailed,
+        SearchTimeOut,
 
         // Check.
         /////////
-        GP_CHECK = 0x0E00,
-        GP_CHECK_BAD_EMAIL,
-        GP_CHECK_BAD_NICK,
-        GP_CHECK_BAD_PASSWORD,
+        Check = 0x0E00,
+        CheckBadMail,
+        CheckBadNick,
+        CheckBadPassword,
 
         // Revoke.
         //////////
-        GP_REVOKE = 0x0F00,
-        GP_REVOKE_NOT_BUDDY,
+        Revoke = 0x0F00,
+        RevokeNotBuddy,
 
         // Registeruniquenick.
         //////////////////////
-        GP_REGISTERUNIQUENICK = 0x1000,
-        GP_REGISTERUNIQUENICK_TAKEN,
-        GP_REGISTERUNIQUENICK_RESERVED,
-        GP_REGISTERUNIQUENICK_BAD_NAMESPACE,
+        RegisterUniquenick = 0x1000,
+        RegisterUniquenickTaken,
+        RegisterUniquenickReserved,
+        RegisterUniquenickBadNamespace,
 
         // Register cdkey.
         //////////////////
-        GP_REGISTERCDKEY = 0x1100,
-        GP_REGISTERCDKEY_BAD_KEY,
-        GP_REGISTERCDKEY_ALREADY_SET,
-        GP_REGISTERCDKEY_ALREADY_TAKEN,
+        RegisterCDKey = 0x1100,
+        RegisterCDKeyBadKey,
+        RegisterCDKeyAlreadySet,
+        RegisterCDKeyAlreadyTaken,
 
         // AddBlock.
         ////////////
-        GP_ADDBLOCK = 0x1200,
-        GP_ADDBLOCK_ALREADY_BLOCKED,
+        AddBlock = 0x1200,
+        AddBlockAlreadyBlocked,
 
         // RemoveBlock.
         ///////////////
@@ -352,6 +352,7 @@ namespace RetroSpyServer.Servers.GPSP.Enumerators
         RemoveBlockNotBlocked,
 
 
-            NoError =0xffff
+        //RetroSpy self defined NoError code
+        NoError = 0xffff
     }
 }
