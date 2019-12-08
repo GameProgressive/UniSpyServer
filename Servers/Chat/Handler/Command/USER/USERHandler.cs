2b90d1d60a0c640ec8f3ba3861b5a03bdb555242
Arves100 <arves100@gmail.com>
2019-12-08 15:10:20 +0100

    Temp parsing of user encoded ip

--- Servers/Chat/Handler/Command/USER/USERHandler.cs
+++ Servers/Chat/Handler/Command/USER/USERHandler.cs
@@ -8,7 +8,18 @@ namespace Chat.Handler.Command.USER
     {
         public static void Handle(ChatSession session, string[] recv)
         {
-            session.chatUserInfo.username = recv[1];
+            int encryptIPSeparator = recv[1].IndexOf('|');
+
+            if (encryptIPSeparator == -1)
+                session.chatUserInfo.username = recv[1];
+            else
+            {
+                // <crypted IP>|<unique nickname ID>
+                string cryptedIP = recv[1].Substring(0, encryptIPSeparator);
+                string uniqueNickID = recv[1].Substring(encryptIPSeparator, recv[1].Length - encryptIPSeparator);
+                Console.WriteLine("Temp {0} {1}", cryptedIP, uniqueNickID);
+            }
+
             session.chatUserInfo.serverIP = recv[3];
         }
     }
