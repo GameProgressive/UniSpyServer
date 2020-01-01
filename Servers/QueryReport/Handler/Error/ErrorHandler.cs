eb2abf8302e02e34c8c91a8098f4c565bf0ffc96
XIAOJIUWO <koujiangheng@live.cn>
2020-01-01 21:46:33 +0800

    minor update of QR

---
+++ Servers/QueryReport/Handler/Error/ErrorHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryReport.Handler.Error
+{
+    public enum QRError : uint
+    {
+        Error,
+        Parse,
+
+
+    }
+}
