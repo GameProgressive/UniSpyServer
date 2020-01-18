c2b1995454d259f4d3cc23b3982f5be0ca24747c
xiaojiuwo <koujiangheng@live.cn>
2020-01-18 20:52:31 +0800

    Changed a parameter name

--- Servers/QueryReport/Handler/CommandSwitcher/CommandSwitcher.cs
+++ Servers/QueryReport/Handler/CommandSwitcher/CommandSwitcher.cs
@@ -12,28 +12,28 @@ namespace QueryReport.Handler.CommandSwitcher
 {
     public class CommandSwitcher
     {
-        public static void Switch(QRServer server, EndPoint endPoint, byte[] message)
+        public static void Switch(QRServer server, EndPoint endPoint, byte[] recv)
         {
             try
             {
-                switch (message[0])
+                switch (recv[0])
                 {
                     case QRClient.Avaliable:
-                        AvailableCheckHandler.BackendAvaliabilityResponse(server, endPoint, message);
+                        AvailableCheckHandler.BackendAvaliabilityResponse(server, endPoint, recv);
                         break;
                     // Note: BattleSpy make use of this despite not being used in both OpenSpy and the SDK.
                     // Perhaps it was present on an older version of GameSpy SDK
                     case QRGameServer.Challenge:
-                        ChallengeHandler.ServerChallengeResponse(server, endPoint, message);
+                        ChallengeHandler.ServerChallengeResponse(server, endPoint, recv);
                         break;
                     case QRClient.Heartbeat: // HEARTBEAT
-                        HeartBeatHandler.HeartbeatResponse(server, endPoint, message);
+                        HeartBeatHandler.HeartbeatResponse(server, endPoint, recv);
                         break;
                     case QRClient.KeepAlive:
-                        KeepAliveHandler.KeepAliveResponse(server, endPoint, message);
+                        KeepAliveHandler.KeepAliveResponse(server, endPoint, recv);
                         break;
                     default:
-                        server.UnknownDataRecived(message);
+                        server.UnknownDataRecived(recv);
                         break;
                 }
             }
