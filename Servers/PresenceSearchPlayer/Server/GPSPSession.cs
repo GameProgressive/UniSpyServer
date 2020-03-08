b86d257699d30cf11acd96b5d16231e713bdceab
xiaojiuwo <koujiangheng@live.cn>
2020-03-08 18:02:04 +0800

    fix an error in gpsp

--- Servers/PresenceSearchPlayer/Server/GPSPSession.cs
+++ Servers/PresenceSearchPlayer/Server/GPSPSession.cs
@@ -20,8 +20,7 @@ namespace PresenceSearchPlayer
                 GameSpyUtils.SendGPError(this, GPErrorCode.Parse, "An invalid request was sended.");
                 return;
             }
-            message =@"\newuser\\email\fridij2007@icloud.com\nick\FridiNaTor\passwordenc\VZ6bkUCsg95I\productid\10618\gamename\fsw10hpc\namespaceid\0\uniquenick\\cdkeyenc\RrTCqwaRq]88Ii7eauCVR[leSA__\id\1\final\";
-
+            
              string[] commands = message.Split("\\final\\",System.StringSplitOptions.RemoveEmptyEntries);
 
             foreach (string command in commands)
