26f9bee8cc40500adf3a896d9af1f26cf26a9969
xiaojiuwo <koujiangheng@live.cn>
2020-03-22 17:41:51 +0800

    update sdkrevision

--- Servers/PresenceConnectionManager/Handler/CommandHandler/General/SDKExtendFeature/SDKRevision.cs
+++ Servers/PresenceConnectionManager/Handler/CommandHandler/General/SDKExtendFeature/SDKRevision.cs
@@ -1,69 +1,48 @@
 ﻿using System;
 using System.Collections;
+using PresenceConnectionManager.Entity.Structure;
 
 namespace PresenceConnectionManager.Handler.General.SDKExtendFeature
 {
     public static class SDKRevision
     {
         /// <summary>
-        /// Extended message support
-        /// </summary>
-        public const uint GPINewAuthNotification = 0b_1;//1
-
-        /// <summary>
-        /// Remove friend from remote
-        /// </summary>
-        public const uint GPINewRevokeNotification = 0b_10;//10
-        /// <summary>
-        /// New Status Info support
-        /// </summary>
-        public const uint GPINewStatusNotification = 0b_100;//1000
-        /// <summary>
-        /// Buddy List + Block List retrieval on login
-        /// </summary>
-        public const uint GPINewListRetrevalOnLogin = 0b_1000;//
-        /// <summary>
-        /// Remote Auth logins now return namespaceid/partnerid on login
-        /// </summary>
-        public const uint GPIRemoteAuthIDSNotification = 0b_10000;
-        /// <summary>
-        /// New CD Key registration style as opposed to using product ids
-        /// </summary>
-        public const uint GPINewCDKeyRegistration = 0b_100000;
-
-        /// <summary>
         /// Tell server send back extra information according to the number of  sdkrevision
         /// </summary>
         public static void ExtendedFunction(GPCMSession session)
         {
-            var bits = new BitArray(BitConverter.GetBytes(session.UserInfo.SDKRevision));
+            if (session.UserInfo.SDKRevision == 0)
+            {
+                session.ToLog(GameSpyLib.Logging.LogLevel.Error, "[SDKRev] No sdkrevision!");
+                return;
+            }
 
-            if (bits[0])
+            if ((session.UserInfo.SDKRevision ^ (uint)SDKRevisionType.GPINewAuthNotification) != 0)
             {
                 //Send add friend request
             }
 
-            if (bits[1])
+            if ((session.UserInfo.SDKRevision ^ (uint)SDKRevisionType.GPINewRevokeNotification) != 0)
             {
                 //send revoke request
             }
 
-            if (bits[2])
+            if ((session.UserInfo.SDKRevision ^ (uint)SDKRevisionType.GPINewStatusNotification) != 0)
             {
                 //send new status info
             }
-
-            if (bits[3])
+     
+            if ((session.UserInfo.SDKRevision ^ (uint)SDKRevisionType.GPINewListRetrevalOnLogin) != 0)
             {
                 //send buddy list and block list
             }
 
-            if (bits[4])
+            if ((session.UserInfo.SDKRevision ^ (uint)SDKRevisionType.GPIRemoteAuthIDSNotification)!=0)
             {
                 //Remote auth
             }
 
-            if (bits[5])
+            if ((session.UserInfo.SDKRevision ^ (uint)SDKRevisionType.GPINewCDKeyRegistration) != 0)
             {
                 //register cdkey with product id
             }
