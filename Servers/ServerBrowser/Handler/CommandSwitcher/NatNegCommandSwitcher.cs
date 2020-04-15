100b87b122cbec48d9c895f324c5eae335ad457d
xiaojiuwo <koujiangheng@live.cn>
2020-04-15 22:29:14 +0800

    correct names

--- Servers/ServerBrowser/Handler/CommandSwitcher/NatNegCommandSwitcher.cs
+++ Servers/ServerBrowser/Handler/CommandSwitcher/NatNegCommandSwitcher.cs
@@ -8,7 +8,7 @@ namespace ServerBrowser.Handler.CommandSwitcher
 {
     public class NatNegCommandSwitcher
     {
-        public static void Switch(SBSession session, byte[] recv)
+        public static void Switch(SBSession client, byte[] recv)
         {
             //we block this for now because we need @Cryptorx get information for us
 
@@ -18,37 +18,37 @@ namespace ServerBrowser.Handler.CommandSwitcher
             //    return;
             //}
             //check and add client into clientList
-            NatNegClientInfo client =
+            NatNegClientInfo clientInfo =
                 SBServer.ClientList.GetOrAdd(
-                    session.Socket.RemoteEndPoint,
-                    new NatNegClientInfo(session.Socket.RemoteEndPoint)
+                    client.Socket.RemoteEndPoint,
+                    new NatNegClientInfo(client.Socket.RemoteEndPoint)
                     );
-            client.LastPacketTime = DateTime.Now;
+            clientInfo.LastPacketTime = DateTime.Now;
 
             //BytesRecieved[7] is nnpacket.PacketType.
             switch ((NatPacketType)recv[7])
             {
                 case NatPacketType.Init:
-                    new InitHandler(session, client, recv).Handle();
+                    new InitHandler(client, clientInfo, recv).Handle();
                     LogWriter.ToLog(NatPacketType.Init.ToString());
                     break;
                 case NatPacketType.AddressCheck:
-                    new AddressHandler(session, client, recv).Handle();
+                    new AddressHandler(client, clientInfo, recv).Handle();
                     LogWriter.ToLog(NatPacketType.AddressCheck.ToString());
                     break;
                 case NatPacketType.NatifyRequest:
-                    new NatifyHandler(session, client, recv).Handle();
+                    new NatifyHandler(client, clientInfo, recv).Handle();
                     LogWriter.ToLog(NatPacketType.NatifyRequest.ToString());
                     break;
                 case NatPacketType.ConnectAck:
-                    client.IsGotConnectAck = true;
+                    clientInfo.IsGotConnectAck = true;
                     break;
                 case NatPacketType.Report:
-                    new ReportHandler(session, client, recv).Handle();
+                    new ReportHandler(client, clientInfo, recv).Handle();
                     LogWriter.ToLog(NatPacketType.Report.ToString());
                     break;
                 case NatPacketType.ErtAck:
-                    new ErtACKHandler(session, client, recv).Handle();
+                    new ErtACKHandler(client, clientInfo, recv).Handle();
                     LogWriter.ToLog(NatPacketType.ErtAck.ToString());
                     break;
                 default:
