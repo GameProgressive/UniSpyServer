a35756156cfa5f8c79fabf5d66d81ff5afcc83f7
xiaojiuwo <koujiangheng@live.cn>
2020-04-19 00:08:29 +0800

    update chat

---
+++ Servers/Chat/Entity/Structure/ChatCommand/ChatBasic/PING.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Chat.Entity.Structure.ChatCommand.ChatBasic
+{
+    public class PING : ChatCommandBase
+    {
+        public override string BuildCommandString(params string[] param)
+        {
+            return $"{GetType().Name} :Hello from RetroSpy";
+        }
+    }
+}
---
+++ Servers/Chat/Entity/Structure/ChatCommand/ChatBasic/PONG.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Chat.Entity.Structure.ChatCommand.ChatBasic
+{
+    public class PONG : ChatCommandBase
+    {
+       public string EchoMessage { get; protected set; }
+        public PONG(string request) : base(request)
+        {
+        }
+        public override bool Parse()
+        {
+            if (!base.Parse())
+            {
+                return false;
+            }
+            EchoMessage = _longParam;
+            return true;
+        }
+    }
+}
