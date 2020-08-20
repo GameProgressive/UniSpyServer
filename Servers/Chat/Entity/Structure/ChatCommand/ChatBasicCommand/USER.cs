d51244aa7b637c3e8f1f19c105b94bd9c28b5516
Arves100 <arves100@gmail.com>
2020-08-20 03:44:53 +0200

    Correct USER parameter 1 name

--- Servers/Chat/Entity/Structure/ChatCommand/ChatBasicCommand/USER.cs
+++ Servers/Chat/Entity/Structure/ChatCommand/ChatBasicCommand/USER.cs
@@ -5,7 +5,7 @@ namespace Chat.Entity.Structure.ChatCommand
     public class USER : ChatCommandBase
     {
         public string UserName { get; protected set; }
-        public string IPAddress { get; protected set; }
+        public string Hostname { get; protected set; }
         public string ServerName { get; protected set; }
         public string NickName { get; protected set; }
         public string Name { get; protected set; }
@@ -18,7 +18,7 @@ namespace Chat.Entity.Structure.ChatCommand
             }
 
             UserName = _cmdParams[0];
-            IPAddress = _cmdParams[1];
+            Hostname = _cmdParams[1];
             ServerName = _cmdParams[2];
             Name = _longParam;
             return true;
