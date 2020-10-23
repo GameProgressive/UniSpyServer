6303ffef07dfc44c0d74827fb665d0fb4ed96a22
XIAOJIUWO <koujiangheng@live.cn>
2020-10-23 14:45:41 +0800

    Simplify json reading process

--- src/Libraries/GameSpyLib/RetroSpyConfig/ConfigManager.cs
+++ src/Libraries/GameSpyLib/RetroSpyConfig/ConfigManager.cs
@@ -16,15 +16,10 @@ namespace GameSpyLib.RetroSpyConfig
 
         private static void LoadConfigFile()
         {
-            FileStream fstream = File.OpenRead(@"RetroSpyServer.json");
-            fstream.Seek(0, SeekOrigin.Begin);
-            byte[] buffer = new byte[(int)fstream.Length];
-            fstream.Read(buffer, 0, (int)fstream.Length);
-            string strBuffer = Encoding.UTF8.GetString(buffer);
-            //stream.Position = 0;
-            Config = JsonConvert.DeserializeObject<RetroSpyConfig>(strBuffer);
-            fstream.Close();
-
+            using (StreamReader fstream = File.OpenText(@"RetroSpyServer.json"))
+            {
+                Config = JsonConvert.DeserializeObject<RetroSpyConfig>(fstream.ReadToEnd());
+            }
         }
     }
 }
