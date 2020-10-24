94a4f433345da5b638af342945260c8286ade6af
XIAOJIUWO <koujiangheng@live.cn>
2020-10-24 12:20:40 +0800

    Update switcher

--- src/Servers/PresenceConnectionManager/Handler/CommandSwticher/PCMCommandSwitcher.cs
+++ src/Servers/PresenceConnectionManager/Handler/CommandSwticher/PCMCommandSwitcher.cs
@@ -3,6 +3,7 @@ using GameSpyLib.Common.Entity.Interface;
 using GameSpyLib.Logging;
 using GameSpyLib.MiscMethod;
 using PresenceConnectionManager.Entity.BaseClass;
+using PresenceConnectionManager.Entity.Structure;
 using PresenceConnectionManager.Handler.CommandHandler.Buddy;
 using PresenceConnectionManager.Handler.CommandHandler.General;
 using PresenceConnectionManager.Handler.CommandHandler.Profile;
@@ -43,58 +44,56 @@ namespace PresenceConnectionManager.Handler
 
                     switch (recv.Keys.First())
                     {
-                        case "login"://login to retrospy
+                        case PCMRequestName.LogIn://login to retrospy
                             new LoginHandler(session, recv).Handle();
                             break;
-                        case "logout"://logout from retrospy
+                        case PCMRequestName.LogOut://logout from retrospy
                             new LogoutHandler(session, recv).Handle();
                             break;
-                        case "ka":
+                        case PCMRequestName.KeepAlive:
                             new KeepAliveHandler(session, recv).Handle();
                             break;
 
-                        case "getprofile"://get profile of a player
+                        case PCMRequestName.GetPlayerProfile://get profile of a player
                             new GetProfileHandler(session, recv).Handle();
                             break;
-                        case "registernick"://update user's uniquenick
+                        case PCMRequestName.RegisterNickName://update user's uniquenick
                             new RegisterNickHandler(session, recv).Handle();
                             break;
-                        case "newuser"://create an new user
+                        case PCMRequestName.CreateNewUser://create an new user
                             new NewUserHandler(session, recv).Handle();
                             break;
-                        case "updateui"://update a user's email
+                        case PCMRequestName.UpdateUserInformation://update a user's email
                             new UpdateUIHandler(session, recv).Handle();
                             break;
-                        case "updatepro"://update a user's profile
+                        case PCMRequestName.UpdateUserProfile://update a user's profile
                             new UpdateProHandler(session, recv).Handle();
                             break;
-                        case "newprofile"://create an new profile
+                        case PCMRequestName.CreateNewProfile://create an new profile
                             new NewProfileHandler(session, recv).Handle();
                             break;
-                        case "delprofile"://delete profile
+                        case PCMRequestName.DeleteProfile://delete profile
                             break;
-                        case "addblock"://add an user to our block list
+                        case PCMRequestName.AddUserToBlockList://add an user to our block list
                             new AddBlockHandler(session, recv).Handle();
                             break;
-                        case "removeblock":
+                        case PCMRequestName.RemoveUserFromBlockList:
                             new RemoveBlockHandler(session, recv).Handle();
                             break;
 
-                        case "addbuddy"://Send a request which adds an user to our friend list
+                        case PCMRequestName.AddUserToFriendList://Send a request which adds an user to our friend list
                             new AddBuddyHandler(session, recv).Handle();
                             break;
-                        case "delbuddy"://delete a user from our friend list
+                        case PCMRequestName.RemoveUserFromFriendList://delete a user from our friend list
                             new DelBuddyHandler(session, recv).Handle();
                             break;
-                        case "status"://update current logged in user's status info
+                        case PCMRequestName.UpdateUserStatus://update current logged in user's status info
                             new StatusHandler(session, recv).Handle();
                             break;
-                        case "statusinfo":
+                        case PCMRequestName.UpdateUserStatusInformation:
                             throw new NotImplementedException();
-                        //case "inviteto":
-                        //    InviteToHandler.InvitePlayer();
-                        //    break;
-
+                        case PCMRequestName.InviteUserToGame:
+                            throw new NotImplementedException();
                         default:
                             LogWriter.UnknownDataRecieved(message);
                             break;
