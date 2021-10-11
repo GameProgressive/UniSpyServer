1a6b250e4a20b76c5cda6e62d142fa63010925ae
Sporesirius <sporesirius@pm.me>
2021-10-11 22:24:09 +0200

    feat(webserver): add unit tests for Auth

---
+++ src/Servers/WebServer/src/Entity/Structure/Request/AuthRequest/LoginPs3CertRequest.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using WebServer.Abstraction;
+using WebServer.Entity.Contract;
+
+namespace WebServer.Entity.Structure.Request.AuthRequest
+{
+    [RequestContract("LoginPs3Cert")]
+    public class LoginPs3CertRequest : RequestBase
+    {
+        public uint GameId { get; set; }
+        public uint PartnerCode { get; set; }
+        public uint PS3cert { get; set; }
+        public LoginPs3CertRequest(string rawRequest) : base(rawRequest)
+        {
+        }
+
+        public override void Parse()
+        {
+            var gameid = _contentElement.Descendants().Where(p => p.Name.LocalName == "gameid").First().Value;
+            GameId = uint.Parse(gameid);
+            var partnercode = _contentElement.Descendants().Where(p => p.Name.LocalName == "partnercode").First().Value;
+            PartnerCode = uint.Parse(partnercode);
+            var ps3cert = _contentElement.Descendants().Where(p => p.Name.LocalName == "ps3cert").First().Value;
+            PS3cert = uint.Parse(ps3cert);
+        }
+    }
+}
