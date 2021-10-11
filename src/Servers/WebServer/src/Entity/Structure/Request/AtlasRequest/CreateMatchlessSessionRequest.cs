28697bcae4be21c6b34e4b5d884c1d7620695126
Sporesirius <sporesirius@pm.me>
2021-10-11 22:24:00 +0200

    feat(webserver): add unit tests for Atlas

---
+++ src/Servers/WebServer/src/Entity/Structure/Request/AtlasRequest/CreateMatchlessSessionRequest.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using WebServer.Abstraction;
+using WebServer.Entity.Contract;
+
+namespace WebServer.Entity.Structure.Request.AtlasRequest
+{
+    [RequestContract("CreateMatchlessSession")]
+    public class CreateMatchlessSessionRequest : RequestBase
+    {
+        public string Certificate { get; set; }
+        public string Proof { get; set; }
+        public uint GameId { get; set; }
+        public CreateMatchlessSessionRequest(string rawRequest) : base(rawRequest)
+        {
+        }
+
+        public override void Parse()
+        {
+            var certificate = _contentElement.Descendants().Where(p => p.Name.LocalName == "certificate").First().Value;
+            Certificate = certificate;
+            var proof = _contentElement.Descendants().Where(p => p.Name.LocalName == "proof").First().Value;
+            Proof = proof;
+            var gameid = _contentElement.Descendants().Where(p => p.Name.LocalName == "gameid").First().Value;
+            GameId = uint.Parse(gameid);
+        }
+    }
+}
---
+++ src/Servers/WebServer/src/Entity/Structure/Request/AtlasRequest/SetReportIntentionRequest.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using WebServer.Abstraction;
+using WebServer.Entity.Contract;
+
+namespace WebServer.Entity.Structure.Request.AtlasRequest
+{
+    [RequestContract("SetReportIntention")]
+    public class SetReportIntentionRequest : RequestBase
+    {
+        public string Certificate { get; set; }
+        public string Proof { get; set; }
+        public uint CsId { get; set; }
+        public uint CcId { get; set; }
+        public uint GameId { get; set; }
+        public string Authoritative { get; set; }
+        public SetReportIntentionRequest(string rawRequest) : base(rawRequest)
+        {
+        }
+
+        public override void Parse()
+        {
+            var certificate = _contentElement.Descendants().Where(p => p.Name.LocalName == "certificate").First().Value;
+            Certificate = certificate;
+            var proof = _contentElement.Descendants().Where(p => p.Name.LocalName == "proof").First().Value;
+            Proof = proof;
+            var csid = _contentElement.Descendants().Where(p => p.Name.LocalName == "csid").First().Value;
+            CsId = uint.Parse(csid);
+            var ccid = _contentElement.Descendants().Where(p => p.Name.LocalName == "ccid").First().Value;
+            CcId = uint.Parse(ccid);
+            var gameid = _contentElement.Descendants().Where(p => p.Name.LocalName == "gameid").First().Value;
+            GameId = uint.Parse(gameid);
+            var authoritative = _contentElement.Descendants().Where(p => p.Name.LocalName == "authoritative").First().Value;
+            Authoritative = authoritative;
+        }
+    }
+}
