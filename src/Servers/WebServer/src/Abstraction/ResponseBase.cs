f45e3404d30976b199f9e4b2f25d070a43b22579
xiaojiuwo <koujiangheng@live.cn>
2021-10-17 19:51:16 +0800

    feat(webserver): move objects into SoapXElement

--- src/Servers/WebServer/src/Abstraction/ResponseBase.cs
+++ src/Servers/WebServer/src/Abstraction/ResponseBase.cs
@@ -7,20 +7,15 @@ namespace WebServer.Abstraction
     public abstract class ResponseBase : UniSpyResponseBase
     {
         protected SoapXElement _xElement { get; private set; }
-        protected static XDeclaration _declaration = new XDeclaration("1.0", "utf-8", null);
-        protected static XNamespace _ns1 = "http://gamespy.net/sake";
         public ResponseBase(UniSpyRequestBase request, UniSpyResultBase result) : base(request, result)
         {
         }
         public override void Build()
         {
-            XNamespace soapEnv = "http://schemas.xmlsoap.org/soap/envelope/";
-            _xElement.Add(new XElement(soapEnv + "Envelope",
-                                new XAttribute(XNamespace.Xmlns + "SOAP-ENV", "http://schemas.xmlsoap.org/soap/envelope/"),
-                                new XAttribute(XNamespace.Xmlns + "SOAP-ENC", "http://schemas.xmlsoap.org/soap/encoding/"),
-                                new XAttribute(XNamespace.Xmlns + "xsi", "http://www.w3.org/2001/XMLSchema-instance"),
-                                new XAttribute(XNamespace.Xmlns + "xsd", "http://www.w3.org/2001/XMLSchema")));
-            _xElement.Add(new XElement(soapEnv + "Body"));
+            // Because the response is kind of soap object, so we did not use SoapXElement as a soap object
+            // SoapXElement only acts like XElement
+            _xElement.Add(SoapXElement.SoapElement);
+            _xElement.Add(new XElement(SoapXElement.SoapNamespace + "Body"));
         }
     }
 }
--- src/Servers/WebServer/src/Entity/Structure/SoapXElement.cs
+++ src/Servers/WebServer/src/Entity/Structure/SoapXElement.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Xml.Linq;
 
 namespace WebServer.Entity.Structure
@@ -6,6 +7,13 @@ namespace WebServer.Entity.Structure
     public class SoapXElement : XElement
     {
         private static XDeclaration _declaration = new XDeclaration("1.0", "utf-8", null);
+        public static XNamespace SoapNamespace = "http://schemas.xmlsoap.org/soap/envelope/";
+        public static XElement SoapElement = new XElement(SoapNamespace + "Envelope",
+                                new XAttribute(XNamespace.Xmlns + "SOAP-ENV", "http://schemas.xmlsoap.org/soap/envelope/"),
+                                new XAttribute(XNamespace.Xmlns + "SOAP-ENC", "http://schemas.xmlsoap.org/soap/encoding/"),
+                                new XAttribute(XNamespace.Xmlns + "xsi", "http://www.w3.org/2001/XMLSchema-instance"),
+                                new XAttribute(XNamespace.Xmlns + "xsd", "http://www.w3.org/2001/XMLSchema"));
+        public static XNamespace SakeNamespace = "http://gamespy.net/sake";
         public SoapXElement(XElement other) : base(other) { }
 
         public SoapXElement(XName name) : base(name) { }
@@ -15,6 +23,14 @@ namespace WebServer.Entity.Structure
         public SoapXElement(XName name, object content) : base(name, content) { }
 
         public SoapXElement(XName name, params object[] content) : base(name, content) { }
+        public List<FieldObject> GetArrayObjects()
+        {
+            throw new NotImplementedException();
+        }
+        public XElement SetArrayObjects(List<FieldObject> objects)
+        {
+            throw new NotImplementedException();
+        }
 
         public override string ToString()
         {
