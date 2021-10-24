e1a8f1f26db119d6aaf05f5b53b40dfa7f213af4
Sporesirius <sporesirius@pm.me>
2021-10-24 22:35:22 +0200

    feat(chat): add unit tests for Message

---
+++ src/Servers/Chat/test/UniSpyServer.Servers.Chat.Test/MessageRequestTest.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using UniSpyServer.Chat.Entity.Structure.Request.Channel;
+
+namespace UniSpyServer.Servers.UniSpyServer.Chat.RequestTest
+{
+    public class MessageRequestTest
+    {
+        // TODO: add AboveTheTableMsgRequest
+
+        // TODO: add NoticeRequest
+
+        // TODO: add PrivateMsgRequest
+
+        // TODO: add UnderTheTableMsgRequest
+    }
+}
