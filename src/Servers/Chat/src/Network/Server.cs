9b154c31fab58d807a0efbdb60f8b45cf161e167
xiaojiuwo <koujiangheng@live.cn>
2021-11-26 09:42:27 +0800

    update(unittest): update chathandler unittest

--- src/Servers/Chat/src/Network/Server.cs
+++ src/Servers/Chat/src/Network/Server.cs
@@ -4,13 +4,12 @@ using System.Net;
 using UniSpyServer.UniSpyLib.Abstraction.BaseClass.Network.Tcp.Server;
 namespace UniSpyServer.Servers.Chat.Network
 {
-    public sealed class Server : UniSpyTcpServer
+    public class Server : UniSpyTcpServer
     {
         public Server(Guid serverID, IPEndPoint endpoint) : base(serverID, endpoint)
         {
             SessionManager = new SessionManager();
         }
-
         protected override TcpSession CreateSession() => new Session(this);
     }
 }
--- src/Servers/Chat/test/UniSpyServer.Servers.Chat.Test/Channel/ChannelHandlerTest.cs
+++ src/Servers/Chat/test/UniSpyServer.Servers.Chat.Test/Channel/ChannelHandlerTest.cs
@@ -1,33 +1,23 @@
 using System;
-using System.Text;
-using System.Threading;
-using NetCoreServer;
-using UniSpyServer.Servers.Chat.Application;
-using UniSpyServer.Servers.Chat.Test.Channel;
+using System.Net;
+using UniSpyServer.Servers.Chat.Entity.Structure.Request.General;
+using UniSpyServer.Servers.Chat.Handler.CmdHandler.General;
+using UniSpyServer.Servers.Chat.Network;
 using UniSpyServer.Servers.Chat.Test.General;
 using Xunit;
 
 namespace UniSpyServer.Servers.Chat.Test
 {
-    public class TestTcpClient : TcpClient
+    public class TestServer : Server
     {
-        public bool IsWaitting { get; private set; }
-        public string CurrentMessage { get; private set; }
-        public TestTcpClient(string ip, int port) : base(ip, port)
+        public TestServer() : base(new Guid(), new IPEndPoint(IPAddress.Any, 6667))
         {
         }
-
-        protected override void OnReceived(byte[] buffer, long offset, long size)
-        {
-            IsWaitting = false;
-            CurrentMessage = Encoding.UTF8.GetString(buffer, (int)offset, (int)size);
-        }
-        public override long Send(string text)
-        {
-            IsWaitting = true;
-            return base.Send(text);
-        }
+        public void OnConnected(Session session) => base.OnConnected(session);
+        public void OnDisconnected(Session session) => base.OnDisconnected(session);
+        public Session CreateTestSession() => (Session)base.CreateSession();
     }
+
     public class ChannelHandlerTest
     {
         public ChannelHandlerTest()
@@ -36,16 +26,14 @@ namespace UniSpyServer.Servers.Chat.Test
         [Fact]
         public void JoinTest()
         {
-            var client1 = new TestTcpClient("127.0.0.1", 6667);
-            client1.Connect();
-            client1.ReceiveAsync();
-            client1.Send(GeneralRequests.User);
-            Thread.Sleep(10000);
-            client1.Send(GeneralRequests.Nick);
-            Thread.Sleep(10000);
-            client1.Send(ChannelRequests.Join);
-            Thread.Sleep(10000);
-            // Assert.Equal(1, ServerFactory.Server.SessionManager.SessionPool.Count);
+            var server = new TestServer();
+            var session = server.CreateTestSession();
+            server.OnConnected(session);
+            var userReq = new UserRequest(GeneralRequests.User);
+            var userHandler = new UserHandler(session, userReq);
+            userHandler.Handle();
+            Assert.Equal(1, server.SessionManager.SessionPool.Count);
+
         }
     }
 }
