0907c8252f6b7c2b06889dce160a3f75c4f56d41
xiaojiuwo <koujiangheng@live.cn>
2021-12-06 16:11:29 +0800

    refactor(chat): finished channel message unit-test

--- src/Servers/Chat/test/UniSpyServer.Servers.Chat.Test/Message/MessageRequestTest.cs
+++ src/Servers/Chat/test/UniSpyServer.Servers.Chat.Test/Message/MessageRequestTest.cs
@@ -7,18 +7,24 @@ namespace UniSpyServer.Servers.Chat.Test.Message
     public class MessageRequestTest
     {
 
-        [Fact(Skip = "TODO: add tests")]
+        [Fact]
         public void AboveTheTableMsg()
         {
             var request = new AboveTheTableMsgRequest(MessageRequests.AboveTheTableMsg);
             request.Parse();
+            Assert.Equal(MessageType.ChannelMessage, request.MessageType);
+            Assert.Null(request.NickName);
+            Assert.Equal("#GSP!room!test", request.ChannelName);
         }
 
-        [Fact(Skip = "TODO: add tests")]
+        [Fact]
         public void Notice()
         {
             var request = new NoticeRequest(MessageRequests.Notice);
             request.Parse();
+            Assert.Equal(MessageType.ChannelMessage, request.MessageType);
+            Assert.Null(request.NickName);
+            Assert.Equal("#GSP!room!test", request.ChannelName);
         }
 
         [Fact]
@@ -31,11 +37,14 @@ namespace UniSpyServer.Servers.Chat.Test.Message
             Assert.Equal("#GSP!room!test", request.ChannelName);
         }
 
-        [Fact(Skip = "TODO: add tests")]
+        [Fact]
         public void UnderTheTableMsg()
         {
             var request = new UnderTheTableMsgRequest(MessageRequests.UnderTheTableMsg);
             request.Parse();
+            Assert.Equal(MessageType.ChannelMessage, request.MessageType);
+            Assert.Null(request.NickName);
+            Assert.Equal("#GSP!room!test", request.ChannelName);
         }
     }
 }
