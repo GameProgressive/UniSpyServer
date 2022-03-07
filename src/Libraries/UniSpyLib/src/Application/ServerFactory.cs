794bd65cdd583efa18e4abee627ee1f6f75a0aa5
xiaojiuwo <koujiangheng@live.cn>
2022-03-07 20:14:21 +0800

    refactor: update version

--- src/Libraries/UniSpyLib/src/Application/ServerFactory.cs
+++ src/Libraries/UniSpyLib/src/Application/ServerFactory.cs
@@ -16,7 +16,7 @@ namespace UniSpyServer.UniSpyLib.Abstraction.BaseClass.Factory
         /// <summary>
         /// UniSpy server version
         /// </summary>
-        public static readonly string Version = "0.7.0";
+        public static readonly string Version = "0.7.1";
         /// <summary>
         /// UniSpy server name
         /// </summary>
