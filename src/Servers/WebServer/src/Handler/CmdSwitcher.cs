deedf058890a797381d309ca97bcbe2457d4046b
xiaojiuwo <koujiangheng@live.cn>
2022-03-10 11:09:19 +0800

    refactor(web): add endpoint check

--- src/Servers/WebServer/src/Handler/CmdSwitcher.cs
+++ src/Servers/WebServer/src/Handler/CmdSwitcher.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Xml.Linq;
+using UniSpyServer.Servers.WebServer.Entity.Constant;
 using UniSpyServer.Servers.WebServer.Entity.Contract;
 using UniSpyServer.UniSpyLib.Abstraction.BaseClass;
 using UniSpyServer.UniSpyLib.Abstraction.Interface;
@@ -14,6 +16,11 @@ namespace UniSpyServer.Servers.WebServer.Handler
 
         protected override void ProcessRawRequest()
         {
+            // var uri = new Uri(_rawRequest.Url);
+            // if (WebEndpoints.AvailableEndpoints.Contains(uri.LocalPath))
+            // {
+            //     throw new UniSpyException($"Invalid http path access:{_rawRequest.Url}");
+            // }
             dynamic xelements = XElement.Parse(_rawRequest.Body);
             var name = xelements.FirstNode.FirstNode.Name.LocalName;
             DeserializeRequest(name, _rawRequest);
