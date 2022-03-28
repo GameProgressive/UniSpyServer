064e913c022af7db8cc9ed1956d080750fdac765
Arves100 <arves100@gmail.com>
2022-03-28 23:38:47 +0200

    sake SearchForRecord (Crysis2 test)

---
+++ src/Servers/WebServer/src/Module/Sake/Handler/SearchForRecordHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using UniSpyServer.Servers.WebServer.Abstraction;
+using UniSpyServer.Servers.WebServer.Entity.Contract;
+using UniSpyServer.Servers.WebServer.Entity.Structure;
+using UniSpyServer.Servers.WebServer.Module.Sake.Entity.Structure.Response;
+using UniSpyServer.Servers.WebServer.Module.Sake.Structure.Request;
+using UniSpyServer.UniSpyLib.Abstraction.Interface;
+
+namespace UniSpyServer.Servers.WebServer.Module.Sake.Handler
+{
+    [HandlerContract("SearchForRecords")]
+    internal class SearchForRecordHandler : CmdHandlerBase
+    {
+        protected new SearchForRecordsRequest _request => (SearchForRecordsRequest)base._request;
+        public SearchForRecordHandler(IClient client, IRequest request) : base(client, request)
+        {
+
+        }
+
+        protected override void ResponseConstruct()
+        {
+            _response = new SearchForRecordResponse(_request, null);
+        }
+    }
+}
