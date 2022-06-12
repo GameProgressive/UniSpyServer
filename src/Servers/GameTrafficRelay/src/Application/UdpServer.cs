3627c63c30b9c3ef716aa2b11cc1fe611a3c478c
xiaojiuwo <koujiangheng@live.cn>
2022-06-12 23:20:05 +0800

    fix(gtr): using wrong reference

--- src/Servers/GameTrafficRelay/src/Application/UdpServer.cs
+++ src/Servers/GameTrafficRelay/src/Application/UdpServer.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Net;
-using UniSpyServer.Servers.NatNegotiation.Entity.Structure;
+using UniSpyServer.Servers.GameTrafficRelay.Entity.Structure;
 using UniSpyServer.UniSpyLib.Abstraction.Interface;
 
 namespace UniSpyServer.Servers.GameTrafficRelay.Application
--- src/Servers/GameTrafficRelay/test/GameTest.cs
+++ src/Servers/GameTrafficRelay/test/GameTest.cs
@@ -118,13 +118,45 @@ namespace UniSpyServer.Servers.GameTrafficRelay.Test
         /// <summary>
         /// New ping packet with changed server ip and port
         /// </summary>
-        public void TestName()
+        public void JuliusRealTest()
         {
-            // Given
+            var serverMock = new Mock<IServer>();
+            serverMock.Setup(s => s.ServerID).Returns(new System.Guid());
+            serverMock.Setup(s => s.ServerName).Returns("GameTrafficRelay");
+            serverMock.Setup(s => s.Endpoint).Returns(new IPEndPoint(IPAddress.Any, 99));
+            var clientSessionMock = new Mock<IUdpSession>();
+            clientSessionMock.Setup(s => s.RemoteIPEndPoint).Returns(new IPEndPoint(IPAddress.Parse("79.209.235.252"), 52577));
+            clientSessionMock.Setup(s => s.Server).Returns(serverMock.Object);
+            clientSessionMock.Setup(s => s.ConnectionType).Returns(NetworkConnectionType.Udp);
+            clientSessionMock.Setup(s => s.SessionExistedTime).Returns(new System.TimeSpan(0, 0, 0, 0, 0));
+            var client = new TestClient(clientSessionMock.Object);
 
-            // When
+            var serverSessionMock = new Mock<IUdpSession>();
+            serverSessionMock.Setup(s => s.RemoteIPEndPoint).Returns(new IPEndPoint(IPAddress.Parse("211.83.127.54"), 50816));
+            serverSessionMock.Setup(s => s.Server).Returns(serverMock.Object);
+            serverSessionMock.Setup(s => s.ConnectionType).Returns(NetworkConnectionType.Udp);
+            serverSessionMock.Setup(s => s.SessionExistedTime).Returns(new System.TimeSpan(0, 0, 0, 0, 0));
+            var server = new TestClient(serverSessionMock.Object);
 
-            // Then
+            var clientRequest = new byte[] { 0xFD, 0xFC, 0x1E, 0x66, 0x6A, 0xB2, 0x03, 0x07, 0x00, 0x00, 0x02, 0x9A, 0xA1, 0x61, 0x8D, 0x59, 0x27, 0x66, 0x00, 0x00 };
+            var serverRequest = new byte[] { 0xFD, 0xFC, 0x1E, 0x66, 0x6A, 0xB2, 0x03, 0x07, 0x00, 0x00, 0x02, 0x9A, 0xA1, 0x61, 0x8D, 0x59, 0x27, 0x66, 0x00, 0x00 };
+
+            client.OnReceived(clientRequest);
+            server.OnReceived(serverRequest);
+
+            client.OnReceived(clientRequest);
+            client.OnReceived(clientRequest);
+            server.OnReceived(serverRequest);
+            server.OnReceived(serverRequest);
+            client.OnReceived(clientRequest);
+            server.OnReceived(serverRequest);
+            client.OnReceived(clientRequest);
+            server.OnReceived(serverRequest);
+            client.OnReceived(clientRequest);
+            server.OnReceived(serverRequest);
+
+            Assert.True(client.Info.TrafficRelayTarget.Session.RemoteIPEndPoint.Equals(server.Session.RemoteIPEndPoint));
+            Assert.True(server.Info.TrafficRelayTarget.Session.RemoteIPEndPoint.Equals(client.Session.RemoteIPEndPoint));
         }
     }
 }
