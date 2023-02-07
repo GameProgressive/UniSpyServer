823d87afb052a5d2b82658e4cc01dc9f578f031f
xiaojiuwo <koujiangheng@live.cn>
2023-02-07 15:46:05 +0800

    refactor(gtr): remove duplicated code

--- src/Servers/GameTrafficRelay/src/Entity/ServerStatusReporter.cs
+++ src/Servers/GameTrafficRelay/src/Entity/ServerStatusReporter.cs
@@ -1,6 +1,4 @@
-using System.Net;
 using System;
-using System.Timers;
 using UniSpyServer.Servers.GameTrafficRelay.Controller;
 using UniSpyServer.UniSpyLib.Config;
 using UniSpyServer.UniSpyLib.Extensions;
@@ -10,18 +8,13 @@ namespace UniSpyServer.Servers.GameTrafficRelay.Entity
     internal class ServerStatusReporter
     {
         private EasyTimer _myTimer;
-        private RelayServerInfo _info;
         private RedisClient _redisClient;
+        private UniSpyServerConfig _config;
         public ServerStatusReporter(UniSpyServerConfig config)
         {
             _redisClient = new RedisClient();
-            var clientCount = NatNegotiationController.ConnectionPairs.Values.Count * 2;
-            _info = new RelayServerInfo()
-            {
-                ServerID = config.ServerID,
-                PublicIPEndPoint = config.PublicIPEndPoint,
-                ClientCount = clientCount
-            };
+            _config = config;
+
             UpdateServerInfo();
         }
         public void Start()
@@ -30,9 +23,13 @@ namespace UniSpyServer.Servers.GameTrafficRelay.Entity
         }
         private void UpdateServerInfo()
         {
-            _info.ClientCount = NatNegotiationController.ConnectionPairs.Count * 2;
-            _redisClient.SetValue(_info);
+            var info = new RelayServerInfo()
+            {
+                ServerID = _config.ServerID,
+                PublicIPEndPoint = _config.PublicIPEndPoint,
+                ClientCount = NatNegotiationController.ConnectionPairs.Values.Count * 2
+            };
+            _redisClient.SetValue(info);
         }
-
     }
 }
