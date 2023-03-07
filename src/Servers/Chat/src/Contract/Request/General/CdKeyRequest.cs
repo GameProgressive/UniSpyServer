f8e52bdc3c08eec96d005966b636af71ec7265bf
xiaojiuwo <koujiangheng@live.cn>
2023-03-07 19:27:32 +0800

    refactor(chat): rename classes name

---
+++ src/Servers/Chat/src/Contract/Request/General/CdKeyRequest.cs
@@ -0,0 +1,24 @@
+using UniSpy.Server.Chat.Abstraction.BaseClass;
+
+
+namespace UniSpy.Server.Chat.Contract.Request.General
+{
+    
+    public sealed class CdKeyRequest : RequestBase
+    {
+
+        public string CdKey { get; private set; }
+        public CdKeyRequest(string rawRequest) : base(rawRequest) { }
+        public override void Parse()
+        {
+            base.Parse();
+
+            if (_cmdParams.Count < 1)
+            {
+                throw new Exception.ChatException("The number of IRC cmdParams are incorrect.");
+            }
+
+            CdKey = _cmdParams[0];
+        }
+    }
+}
---
+++ src/Servers/Chat/src/Handler/CmdHandler/General/CdKeyHandler.cs
@@ -0,0 +1,20 @@
+using UniSpy.Server.Chat.Abstraction.BaseClass;
+using UniSpy.Server.Chat.Contract.Request.General;
+using UniSpy.Server.Chat.Contract.Response.General;
+using UniSpy.Server.Core.Abstraction.Interface;
+
+namespace UniSpy.Server.Chat.Handler.CmdHandler.General
+{
+    
+    public sealed class CdKeyHandler : CmdHandlerBase
+    {
+        private new CdKeyRequest _request => (CdKeyRequest)base._request;
+
+        public CdKeyHandler(IClient client, IRequest request) : base(client, request){ }
+
+        protected override void ResponseConstruct()
+        {
+            _response = new CDKeyResponse(_request, _result);
+        }
+    }
+}
