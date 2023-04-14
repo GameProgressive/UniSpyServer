1c2aa40f9ed4d39c4f2006985fd28e038057bf66
xiaojiuwo <koujiangheng@live.cn>
2023-04-14 10:24:19 +0800

    refactor(sb): added comment

--- src/Servers/ServerBrowser/src/V2/Handler/CmdHandler/ServerList/ServerListHandler.cs
+++ src/Servers/ServerBrowser/src/V2/Handler/CmdHandler/ServerList/ServerListHandler.cs
@@ -65,9 +65,12 @@ namespace UniSpy.Server.ServerBrowser.V2.Handler.CmdHandler
         }
         private void P2PGroupRoomList()
         {
+            // we first get the peer rooms in redis
             // Game name is unique in redis database
             var peerRooms = QueryReport.V2.Application.StorageOperation.Persistance.GetPeerRoomsInfo(_request.GameName);
+            // get the peer room in database
             var grouplist = QueryReport.V2.Application.StorageOperation.Persistance.GetGroupList(_request.GameName);
+            // check if there are missing peer rooms in redis
             var addInfos = grouplist.Where(g => peerRooms.All(r => r.GroupId != g.Groupid)).ToList();
             foreach (var room in addInfos)
             {
