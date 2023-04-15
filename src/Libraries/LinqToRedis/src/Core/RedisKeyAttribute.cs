aaffb2618dc1734c2af3dbfcc417997262cd0105
xiaojiuwo <koujiangheng@live.cn>
2023-04-15 23:20:34 +0800

    update: limit rediskey attribute usage

--- src/Libraries/LinqToRedis/src/Core/RedisKeyAttribute.cs
+++ src/Libraries/LinqToRedis/src/Core/RedisKeyAttribute.cs
@@ -7,6 +7,7 @@ namespace UniSpy.LinqToRedis
     /// The key must be simple object, which mean its ToString method can out put a valid string
     /// Such as int, string, DateTime, Guid, IPEndPoint, etc.
     /// </summary>
+    [AttributeUsage(validOn: AttributeTargets.Property)]
     public class RedisKeyAttribute : Attribute
     {
         public RedisKeyAttribute() { }
